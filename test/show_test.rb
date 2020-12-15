c0763d02768de3901528e7e413c204c66f1e199e
Katy La Tour <klatour324@gmail.com>
2020-12-15 10:23:50 -0600

    Create test for total salary

--- test/show_test.rb
+++ test/show_test.rb
@@ -12,20 +12,20 @@ class ShowTest < Minitest::Test
 
     assert_instance_of Show, knight_rider
   end
+
+  def test_it_has_a_total_salary
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal 2600000, knight_rider.total_salary
+  end
 end
 
 
 
 
-# pry(main)> knight_rider.name
-# # => "Knight Rider"
-#
-# pry(main)> knight_rider.creator
-# # => "Glen Larson"
-#
-# pry(main)> knight_rider.characters
-# # => [#<Character:0x00007f8327213de0...>, #<Character:0x00007f8326ab57d8...>]
-#
+
 # pry(main)> knight_rider.total_salary
 # # => 2600000
 #
