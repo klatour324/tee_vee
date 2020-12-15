8caaf4bf1994c93f6c92686532b6f50d52d86937
Katy La Tour <klatour324@gmail.com>
2020-12-15 11:17:52 -0600

    Add main_characters method to Network class

--- lib/network.rb
+++ lib/network.rb
@@ -10,4 +10,16 @@ class Network
   def add_show(show)
     @shows.push(show)
   end
+
+  def main_characters
+    main_actor_names = []
+    @shows.each do |show|
+      show.characters.select do |character|
+        if character.name == character.name.upcase && character.salary > 500_000
+          main_actor_names << character
+        end
+      end
+    end
+    main_actor_names
+  end
 end
