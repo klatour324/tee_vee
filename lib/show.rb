e9ede60c6003dd3a2d344f858df2bb3405de4250
Katy La Tour <klatour324@gmail.com>
2020-12-15 10:24:20 -0600

    Add total salary method to Show class

--- lib/show.rb
+++ lib/show.rb
@@ -8,4 +8,12 @@ class Show
     @creator = creator
     @characters = characters
   end
+
+  def total_salary
+    total_amount = 0
+    @characters.each do |character|
+      total_amount += character.salary
+    end
+    total_amount
+  end
 end
